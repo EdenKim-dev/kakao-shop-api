735e576bc0f5e436a4e733dc8b2c428a8c60fe19
eden.kim <108042383+EdenKim-dev@users.noreply.github.com>
2023-07-28 15:16:53 +0900

    Update startClient.sh

--- startClient.sh
+++ startClient.sh
@@ -1,4 +1,5 @@
-rm -rf /var/lib/apt/lists/* && \
-rm /etc/nginx/sites-enabled/default && \
+rm -rf /var/lib/apt/lists/*
+rm /etc/nginx/sites-enabled/default
 service nginx start
+service mysql start
 (cd /goormService/frontend && npm i && npm start)
